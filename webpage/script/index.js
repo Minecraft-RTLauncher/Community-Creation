d2984af91bb655236d293d021bc9aa5ac3373a10
Twisuki <Twisuki@outlook.com>
2025-05-05 15:42:48 +0800

    禁用图片拖拽

--- webpage/script/index.js
+++ webpage/script/index.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () => {
 	document.body.classList.add("loaded");
 });
+
+// 禁止图片拖拽
+document.addEventListener('DOMContentLoaded', function () {
+	const images = document.querySelectorAll('img');
+	images.forEach(img => {
+		img.addEventListener('dragstart', function (e) {
+			e.preventDefault();
+		});
+	});
+});
